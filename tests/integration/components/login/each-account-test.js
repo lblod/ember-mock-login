34a39a097f0b239e1d67b27c113f41deb54a2ba3
Erika Pauwels <erika.pauwels@gmail.com>
2018-09-27 17:49:35 +0200

    Run codemods

--- tests/integration/components/login/each-account-test.js
+++ tests/integration/components/login/each-account-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | login/each-account', function(hooks) {
 
     await render(hbs`{{login/each-account}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | login/each-account', function(hooks) {
       {{/login/each-account}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
--- tests/integration/components/mock-logout-test.js
+++ tests/integration/components/mock-logout-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | mock-logout', function(hooks) {
 
     await render(hbs`{{mock-logout}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | mock-logout', function(hooks) {
       {{/mock-logout}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
