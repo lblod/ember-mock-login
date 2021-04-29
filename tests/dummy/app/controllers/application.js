8d27d19b391e900ad2a97717cfe0d48e70bae240
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-04-29 17:25:54 +0200

    Update the dummy application

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,33 +1,34 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import EmberObject from '@ember/object';
 
-export default Controller.extend({
-  session: service('session'),
-  // eslint-disable-next-line ember/avoid-leaking-state-in-ember-objects
-  accounts: [
-    {
+export default class ApplicationController extends Controller {
+  @service session;
+
+  accounts = [
+    EmberObject.create({
       id: 'e842a393-1252-4e06-bab4-18df36c53808',
       provider: 'mock',
-      gebruiker: {
+      gebruiker: EmberObject.create({
         id: '1',
         firstName: 'Jane',
         lastName: 'Doe',
-        group: {
+        group: EmberObject.create({
           id: '5ac63f89ea27dc000101cf74'
-        }
-      }
-    },
-    {
+        })
+      })
+    }),
+    EmberObject.create({
       id: "7ed41362-e636-4bf2-826c-7ee476346890",
       provider: 'mock',
-      gebruiker: {
+      gebruiker: EmberObject.create({
         id: '2',
         firstName: 'Jimmy',
         lastName: 'Janssens',
-        group: {
+        group: EmberObject.create({
           id: '5ac63f89ea27dc000101cf7c'
-        }
-      }
-    }
+        })
+      })
+    })
   ]
-});
+}
