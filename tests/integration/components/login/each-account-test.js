7be555cb3a75e3aa7cc10c78f87f0ac1f0aa874d
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-12-04 10:42:58 +0100

    Fix a failing test

--- tests/integration/components/login/each-account-test.js
+++ tests/integration/components/login/each-account-test.js
@@ -7,15 +7,14 @@ module('Integration | Component | login/each-account', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     this.set('accounts', [
       "test",
       "account"
     ])
+    this.noop = () => {};
+
     await render(hbs`
-      <Login::EachAccount @accounts={{this.accounts}} as |account|>
+      <Login::EachAccount @login={{this.noop}} @accounts={{this.accounts}} as |account|>
         {{account}}
       </Login::EachAccount>
     `);
