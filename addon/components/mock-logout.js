0b9d065dae89f1ce431340d35d737f9e8e3efa17
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-12-04 14:36:40 +0100

    Deprecate the `<MockLogout>` and `<login.each-account>` components

--- addon/components/mock-logout.js
+++ addon/components/mock-logout.js
@@ -1,9 +1,27 @@
 import Component from '@glimmer/component';
+import { deprecate } from '@ember/debug';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class MockLogoutComponent extends Component {
   @service session;
+
+  constructor() {
+    super(...arguments);
+
+    deprecate(
+      'The <MockLogout> component is deprecated. Use the `invalidate` method of the `session` service instead.',
+      false,
+      {
+        id: '@lblod/ember-mock-login.mock-logout-component',
+        for: '@lblod/ember-mock-login',
+        since: {
+          enabled: '0.8.0',
+        },
+        until: '0.9.0',
+      }
+    );
+  }
 
   @action
   logout() {
